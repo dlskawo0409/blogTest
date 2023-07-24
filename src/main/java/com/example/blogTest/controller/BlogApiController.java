d599e2bd8f6f8a35c6baabf4a065b18b45d2d5c8
dlskawo0409 <dlskawo0409@gmail.com>
2023-07-24 20:58:52 +0900

    add h2

--- src/main/java/com/example/blogTest/controller/BlogApiController.java
+++ src/main/java/com/example/blogTest/controller/BlogApiController.java
@@ -4,8 +4,10 @@ import com.example.blogTest.domain.Article;
 import com.example.blogTest.dto.AddArticleRequest;
 import com.example.blogTest.service.BlogService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlogApiController {
 
     private final BlogService blogService;
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 
     @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){

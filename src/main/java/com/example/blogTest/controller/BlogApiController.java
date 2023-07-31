d20e53afc5cdf5c2e2cd5357b5696ffc0aa5a2ea
dlskawo0409 <dlskawo0409@gmail.com>
2023-07-31 10:03:28 +0900

    add findAll

--- src/main/java/com/example/blogTest/controller/BlogApiController.java
+++ src/main/java/com/example/blogTest/controller/BlogApiController.java
@@ -2,15 +2,17 @@ package com.example.blogTest.controller;
 
 import com.example.blogTest.domain.Article;
 import com.example.blogTest.dto.AddArticleRequest;
+import com.example.blogTest.dto.ArticleResponse;
 import com.example.blogTest.service.BlogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,5 +30,14 @@ public class BlogApiController {
         Article saveArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saveArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllAriticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok().body(articles);
     }
 }
